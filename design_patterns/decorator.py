86ff10271b4782eb3dd990c88e845241d92e3131
Chiaope <90232009+Chiaope@users.noreply.github.com>
2026-06-24 04:27:07 +0000

    updated decorator

--- design_patterns/decorator.py
+++ design_patterns/decorator.py
@@ -1,28 +1,34 @@
 from functools import wraps
+import time
+import random
 
 """
 The main purpose of decorators is to make sure something is ran before the actual function is ran
 """
 
 
-def my_wrapper(func):
+def timer_wrapper(func):
     @wraps(func)  # This is needed to preserve the original function's metadata
     def decorator_function(*args, **kwargs):
-        print(
-            "hehe I am a decorator"
-        )  # Run whatever the decorator is supposed to run before running the actual function
-        return func(*args, **kwargs)  # Run the function and returns it
+        start = time.perf_counter()  # Run whatever the decorator is supposed to run before running the actual function
+        results = func(*args, **kwargs)  # Run the function and save it or we can return it directly
+        time_spend = time.perf_counter() - start  # Run any function that is after the actual function
+        print(f"Function ran for {time_spend:.3f} seconds.")
+        return results
+
 
     return decorator_function  # Return the decorator function without the brackets so it doesnt get ran until it is decorated
 
 
-@my_wrapper
-def do_something(message):
-    """My job is to do something"""
-    print(message)
+@timer_wrapper
+def just_sleep(start, end):
+    """My job is to sleep"""
+    print("Going to sleep")
+    time.sleep(random.uniform(start,end))
+    return "Woken up"
 
 
 if __name__ == "__main__":
-    do_something("my message")
-    print(do_something.__doc__)
-    print(do_something.__name__)
+    print(just_sleep(1, 3))
+    print(just_sleep.__doc__)
+    print(just_sleep.__name__)
