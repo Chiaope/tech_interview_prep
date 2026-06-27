ba5f87978b0d77153e17600507801cc141637a26
Chiaope <gui_kang@hotmail.com>
2026-06-27 22:40:24 +0800

    added comments so i will understand in the future hopefully lol

--- algo/sliding_window.py
+++ algo/sliding_window.py
@@ -18,12 +18,22 @@ if __name__ == "__main__":
     k = 3
     dq = deque()
     results = []
+    # iterate through the indexes of the array because we only want the index
+    # using index will speed up the process because we can use the index to get the value from the array
     for i in range(len(arr)):
+        # check if left most index is out of the window, if so pop it from the left
         if len(dq) > 0 and dq[0] < i - k + 1:
             dq.popleft()
+        # the right most index is the smallest value in the window
+        # if the current value is greater than the right most value
+        # pop it from the right
         while len(dq) > 0 and arr[i] > arr[dq[-1]]:
             dq.pop()
+        # once removed all the smaller values from the right
+        # add the current index to the right
         dq.append(i)
+        # check if we have at least k elements in the window
         if i >= k - 1:
+            # the left most index is the largest value in the window
             results.append(arr[dq[0]])
     print(results)
